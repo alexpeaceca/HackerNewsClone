f72881c5d7222b665e5ef439aac70133e5ae2ca4
Alex Peace <alxpeace@gmail.com>
2018-11-04 20:12:58 -0800

    Hook into API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HackerNewsAPIService } from './hackernews-api.service';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,7 +24,7 @@ import { ItemComponent } from './item/item.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
